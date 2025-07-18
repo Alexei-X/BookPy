edf62d247fa1e8c896d7fc0dc119e55ccd8bea3a
alexbataille <alex.bataille@ulb.be>
2025-07-18 15:30:29 +0200

    title in MainMenuView.py

--- src/main.py
+++ src/main.py
@@ -3,3 +3,5 @@ from views.MainMenuView import MainMenuView
 if __name__ == "__main__":
     print("BookPy starting...")
     mainMenuView = MainMenuView()
+    mainMenuView.display_basic_elements()
+    mainMenuView.run()
--- src/views/MainMenuView.py
+++ src/views/MainMenuView.py
@@ -6,7 +6,14 @@ class MainMenuView:
         self.root.title("BookPy")
         self.root.geometry("1280x720")
         self.root.resizable(width=False, height=False)
-        self.root.mainloop()
 
     def stop(self):
         self.root.destroy()
+
+    def run(self):
+        self.root.mainloop()
+
+    def display_basic_elements(self):
+
+        titleLabel = tk.Label(self.root, text="BookPy", font=("Arial bold", 24))
+        titleLabel.pack(ipady=20)
